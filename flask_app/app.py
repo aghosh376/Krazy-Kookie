e5e5ee7b6c698572ee6dc1c4be42c854223e0d58
aghosh376 <arayboghosh@gmail.com>
2024-03-09 08:45:54 -0800

    Connected flask to firebase

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template
+from firebase import firebase
+
+firebase = firebase.FirebaseApplication('https://hackmercedix-5aa06-default-rtdb.firebaseio.com/', None)
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
+  result = firebase.get('/countries', None)
+  return render_template("index.html", places=result)
+
+@app.route("/members")
+def members():
+   return {"members": ["Member 1", "Members 2", "Members 3"]}
+
+@app.route('/test')
+def test():
    return render_template('index.html')
+
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
