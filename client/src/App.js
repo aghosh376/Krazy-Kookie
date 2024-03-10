bed6f1a12f197fb2344af50b4fb828be800fcfb1
E.K <fake.hack376@gmail.com>
2024-03-10 04:07:43 -0700

    React and Flask integration attempt
    Integrating Flask and React based upon this tutorial https://www.youtube.com/watch?v=7LNl2JlZKHA

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header'
-import Features from './components/Features'
-import Footer from './components/Footer'
+import React, {useState, useEffect} from 'react';
 
+/*
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Features/>
-      <Footer/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+} */
+
+function App() {
+
+  const[data, setData] = useState([{}]);
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+  
+  return (
+    <div>
+      {(typeof data.members === 'undefined') ? (
+        <p>Loading...</p>
+      ) : ( 
+          data.members.map((members, i) => {
+            <p key={i}>{members}</p> 
+            })
+      )}
     </div>
   );
 }
